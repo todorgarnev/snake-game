526647e0c83ee241e4940687b84daf87b9515ec6
Todor Garnev <tgarnev@abv.bg>
2021-01-15 20:55:14 +0200

    Updated style; Added filed cells; Added snake's start position.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+enum cellType {
+  empty = "empty",
+  target = "target",
+  snake = "snake"
+}
+interface Cell {
+  x: number;
+  y: number;
+  type: cellType;
+}
+
 function App() {
+  const [field, setField] = useState<Cell[]> ([]);
+
+  useEffect(() => {
+    const arr: Cell[] = [];
+
+    for (let i = 0; i < 25; i++) {
+      for (let y = 0; y < 25; y++) {
+        let type: cellType = cellType.empty
+        let cell: Cell;
+
+        if (i === 12 && y === 12) {
+          cell = {
+            x: i,
+            y: y,
+            type: type
+          };
+        }
+
+        arr.push(cell);
+      }
+    }
+    setField(arr);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Snake game</h1>
+
+      <div className="field">
+        {field.map((cell: Cell, index: number) => <div className={`cell ${cell.type}`} key={index}></div>)}
+      </div>
     </div>
   );
 }
