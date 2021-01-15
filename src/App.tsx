595e7ceed008ed33d8d6422a8261308ddbdd4007
Todor Garnev <tgarnev@abv.bg>
2021-01-15 21:29:57 +0200

    Added eventListener for arrow buttons.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 enum cellType {
   empty = "empty",
@@ -13,29 +13,62 @@ interface Cell {
 }
 
 function App() {
-  const [field, setField] = useState<Cell[]> ([]);
+  const [field, setField] = useState<Cell[]>([]);
+  const [snakePosition, setSnakePosition] = useState<Cell | null>(null);
 
   useEffect(() => {
     const arr: Cell[] = [];
 
     for (let i = 0; i < 25; i++) {
       for (let y = 0; y < 25; y++) {
-        let type: cellType = cellType.empty
+        let type: cellType = cellType.empty;
         let cell: Cell;
 
         if (i === 12 && y === 12) {
-          cell = {
+          type = cellType.snake
+          setSnakePosition({
             x: i,
             y: y,
             type: type
-          };
+          });
         }
+
+        cell = {
+          x: i,
+          y: y,
+          type: type
+        };
 
         arr.push(cell);
       }
     }
+    console.log(arr);
     setField(arr);
+
+    document.addEventListener("keydown", handleKeyPress);
+    return function cleanup() {
+      document.removeEventListener("keydown", handleKeyPress);
+    }
   }, []);
+
+  const handleKeyPress = (event: KeyboardEvent): void => {
+    switch (event.key) {
+      case "ArrowUp":
+        console.log("UP")
+        break;
+      case "ArrowDown":
+        console.log("DOWN")
+        break;
+      case "ArrowRight":
+        console.log("RIGHT")
+        break;
+      case "ArrowLeft":
+        console.log("LEFT")
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
     <div className="app">
