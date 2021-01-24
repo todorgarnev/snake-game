1ace47a211d3d6624363a52f8efbf93c370661a5
Todor Garnev <tgarnev@abv.bg>
2021-01-24 22:59:40 +0200

    Added correct moveSnake functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { Cell, cellType, Direction, ArrowType } from "./models";
-import { getCell, getField, isInField, updateField, isFoodEaten } from "./utils";
+import { getCell, getField, isInField, updateField, isFoodEaten, moveSnake } from "./utils";
 import useInterval from "./useInterval";
 import "./App.css";
 
 function App() {
   const [field, setField] = useState<Cell[]>(getField(25));
   const [food, setFood] = useState<Cell>(getCell(cellType.food));
-  const [snakeHead, setSnakeHead] = useState<Cell>(getCell(cellType.snake));
-  const [prevSnakeHead, setPrevSnakeHead] = useState<Cell>(snakeHead);
-  const [snakeBody, setSnakeBody] = useState<Cell[]>([]);
+  const [snakeHead, setSnakeHead] = useState<Cell[]>([getCell(cellType.snake)]);
+  const [prevSnakeHead, setPrevSnakeHead] = useState<Cell>(snakeHead[0]);
+  // const [snakeBody, setSnakeBody] = useState<Cell[]>([]);
   const [direction, setDirection] = useState<Direction>(Direction.Right);
   const [isGameOn, setIsGameOn] = useState<boolean>(true);
 
@@ -43,7 +43,7 @@ function App() {
 
   useEffect(() => {
     document.addEventListener("keydown", handleKeyPress);
-    setField(updateField(field, food, snakeHead));
+    setField(updateField(field, food, snakeHead[0]));
 
     return () => {
       document.removeEventListener("keydown", handleKeyPress);
@@ -51,10 +51,10 @@ function App() {
   }, [handleKeyPress]);
 
   useEffect(() => {
-    if (isInField(snakeHead, field)) {
-      if (isFoodEaten(snakeHead, food)) {
-        setSnakeBody((prevSnakeBody: Cell[]) => [
-          ...prevSnakeBody,
+    if (isInField(snakeHead[0], field)) {
+      if (isFoodEaten(snakeHead[0], food)) {
+        setSnakeHead((prevSnakeHead: Cell[]) => [
+          ...prevSnakeHead,
           {
             x: food.x,
             y: food.y,
@@ -63,49 +63,18 @@ function App() {
         ]);
         setFood(getCell(cellType.food));
       }
-      setField(updateField(field, food, snakeHead, prevSnakeHead, ...snakeBody));
+      setField(updateField(field, food, prevSnakeHead, ...snakeHead));
     } else {
       setIsGameOn(false);
     }
   }, [snakeHead]);
 
   useInterval(() => {
+    setSnakeHead(moveSnake(snakeHead, direction));
     setPrevSnakeHead({
-      x: snakeHead.x,
-      y: snakeHead.y,
+      x: snakeHead[snakeHead.length - 1].x,
+      y: snakeHead[snakeHead.length - 1].y,
       type: cellType.empty
-    });
-
-    setSnakeHead((prevSnakeHead: Cell) => {
-      switch (direction) {
-        case Direction.Top:
-          return {
-            x: prevSnakeHead.x - 1,
-            y: prevSnakeHead.y,
-            type: prevSnakeHead.type
-          };
-        case Direction.Right:
-          return {
-            x: prevSnakeHead.x,
-            y: prevSnakeHead.y + 1,
-            type: prevSnakeHead.type
-          };
-        case Direction.Down:
-          return {
-            x: prevSnakeHead.x + 1,
-            y: prevSnakeHead.y,
-            type: prevSnakeHead.type
-          };
-        case Direction.Left:
-          return {
-            x: prevSnakeHead.x,
-            y: prevSnakeHead.y - 1,
-            type: prevSnakeHead.type
-          };
-        default:
-          break;
-      }
-      return prevSnakeHead;
     });
 }, isGameOn ? 150 : null);
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { Cell, cellType } from "./models";
+import { Cell, cellType, Direction } from "./models";
 
 export const getField = (fieldSize: number): Cell[] => {
   const arr: Cell[] = [];
@@ -43,3 +43,44 @@ export const updateField = (field: Cell[], ...cells: Cell[]): Cell[] => {
 export const isInField = (cell: Cell, field: Cell[]): boolean => field.some((fieldCell: Cell) => fieldCell.x === cell.x && fieldCell.y === cell.y);
 
 export const isFoodEaten = (snakeHead: Cell, food: Cell): boolean => snakeHead.x === food.x && snakeHead.y === food.y;
+
+export const moveSnake = (snake: Cell[], direction: Direction): Cell[] => {
+  console.log(snake);
+
+  return snake.map((cell: Cell, index) => {
+    let updatedCell: Cell = cell;
+
+    if (index === 0) {
+      switch (direction) {
+        case Direction.Top:
+          return {
+            x: updatedCell.x - 1,
+            y: updatedCell.y,
+            type: updatedCell.type
+          }
+        case Direction.Right:
+          return {
+            x: updatedCell.x,
+            y: updatedCell.y + 1,
+            type: updatedCell.type
+          }
+        case Direction.Down:
+          return {
+            x: updatedCell.x + 1,
+            y: updatedCell.y,
+            type: updatedCell.type
+          }
+        case Direction.Left:
+          return {
+            x: updatedCell.x,
+            y: updatedCell.y - 1,
+            type: updatedCell.type
+          }
+        default:
+          return updatedCell
+      }
+    }
+
+    return snake[index - 1];
+  });
+}
